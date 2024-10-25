195852d4a891e3442f2c5da9c18873460ded7dd7
arilloid <arinak1017@gmail.com>
2024-10-25 22:08:47 +0000

    impoved comments, formatted

--- lab4-aarch64/loop.s
+++ lab4-aarch64/loop.s
@@ -1,50 +1,56 @@
 .text
 .globl _start
-
-min = 0                          /* Starting value for the loop index (constant) */
-max = 31                         /* Loop exits when the index hits this number (i < max) */
-
+min = 0                          /* starting value for the loop index; **note that this is a symbol (constant)**, not a variable */
+max = 31                         /* loop exits when the index hits this number (loop condition is i<max) */
+divisor = 10                     /* divisor for 2-digit conversion */
 _start:
-    mov     x19, min            /* Initialize loop counter (x19) with starting value (0) */
+    /*
+        register assignmnets
+        ---------------------
+        r19 = loop index  
+        r20 = divisor
+        r21 = 1st digit of loop index
+        r22 = 2nd digit of loop index
+        r23 = adress of the digit in the message buffer
+     */
+    mov     x19, min   
+    mov     x20, divisor   
+    loop:
+        /* divide loop index by 10 to find the first digit (quotient) */
+        udiv    x21, x19, x20       /* quotient for first digit = x21 = index / 10 */
+        
+        /* calculate the remainder using msub */
+        msub    x22, x21, x20, x19  /* x22 = index - (quotient * divisor) */
 
-loop:
-    /* Load divisor (10) into a register */
-    mov     x22, 10             /* Load the constant 10 into x22 */
+        /* convert first digit to ASCII */
+        add     w21, w21, #48       /* convert the 1st digit to ASCII */
+        adr     x23, msg_digit_1    /* load address of the first digit placeholder in the message buffer */
+        strb    w21, [x23]          /* store the first digit in the message buffer */
 
-    /* Divide loop index by 10 to find the first digit (quotient) */
-    udiv    x20, x19, x22       /* x20 = x19 / 10 (quotient for first digit) */
-    
-    /* Calculate the remainder using msub */
-    msub    x21, x20, x22, x19  /* x21 = x19 - (x20 * 10); use xzr for zero register to ensure correct operation */
+        /* convert second digit to ASCII */
+        add     w22, w22, #48       /* convert the 2nd digit to ASCII */
+        adr     x23, msg_digit_2    /* load address of the second digit placeholder in the message buffer */
+        strb    w22, [x23]          /* store the second digit in the message buffer */
 
-    /* Convert first digit to ASCII */
-    add     w20, w20, #48       /* Convert integer in x20 to ASCII ('0' = 48) */
-    adr     x23, msg_digit       /* Load address of the first digit placeholder in the message buffer */
-    strb    w20, [x23]          /* Store the first digit in the message buffer */
+        /* write message to stdout */
+        mov     x0, 1       /* file descriptor: 1 is stdout */
+        adr     x1, msg   	/* message location (memory address) */
+        mov     x2, len   	/* message length (bytes) */
+        mov     x8, 64     	/* write is syscall #64 */
+        svc     0          	/* invoke syscall */
 
-    /* Convert second digit to ASCII */
-    add     w21, w21, #48       /* Convert integer in x21 to ASCII ('0' = 48) */
-    adr     x25, msg_digit + 1   /* Load address of the second digit placeholder in the message buffer */
-    strb    w21, [x25]          /* Store the second digit in the message buffer */
+        /* looping */
+        add     x19, x19, 1     /* increment the loop counter */
+        cmp     x19, max        /* see if we've hit the max */
+        b.ne    loop            /* if not, then continue the loop */
 
-    /* Write message to stdout */
-    mov     x0, 1                /* File descriptor: 1 corresponds to stdout */
-    adr     x1, msg              /* Load address of the message to be printed */
-    mov     x2, len              /* Load the length of the message */
-    mov     x8, 64               /* Specify syscall number for write (syscall #64) */
-    svc     0                    /* Invoke syscall to write the message */
-
-    /* Increment loop counter and check for loop continuation */
-    add     x19, x19, 1          /* Increment loop counter (x19) by 1 */
-    cmp     x19, max             /* Compare loop counter with the maximum value */
-    b.ne    loop                 /* Branch to 'loop' if the counter has not reached max */
-
-    /* Exit the program after completing the loop */
-    mov     x0, 0                /* Set exit status to 0 (indicates successful termination) */
-    mov     x8, 93               /* Specify syscall number for exit (syscall #93) */
-    svc     0                    /* Invoke syscall to exit the program */
+        /* exit the program after loop exit */
+        mov     x0, 0           /* set exit status to 0 */
+        mov     x8, 93          /* exit is syscall #93 */
+        svc     0               /* invoke syscall */
 
 .data
-msg:     .ascii      "Loop: ##\n"  /* Message template containing placeholders for the two digits */
-msg_digit = msg + 6              /* Position of the first digit placeholder in the message (6 bytes in) */
-len =    . - msg                 /* Calculate the length of the message for the write syscall */
+msg: 	.ascii      "Loop: ##\n"
+msg_digit_1 = msg + 6     /* position of the digit placeholder */
+msg_digit_2 = msg + 7
+len= 	. - msg
